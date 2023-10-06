3616e48b2b73ba03ec69ab44b717531a2b3c5ecd
Ashley <ashley.sands01@gmail.com>
2023-10-06 14:21:09 -0400

    setup database

---
+++ config/db.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose'
+import 'dotenv/config'
+
+const localdb = 'mongodb://127.0.0.1:27017/organizerDatabase'
+
+const MONGODB_URI = process.env.MONGODB_URI
+
+const currentDB = process.env.DEPLOYED ? localdb : MONGODB_URI
+
+mongoose.connect(currentDB)
+
+const db = mongoose.connection
+
+export default db
